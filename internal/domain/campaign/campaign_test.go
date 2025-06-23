8e9a302da03605a2f5ac01daf01d2ddc40b671c4
Edmar Filho <edmarfilho2013@gmail.com>
2025-06-23 16:46:11 -0300

    test: realizando testes unitários na struct Campaign

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -1,20 +1,42 @@
 package campaign
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestNewCampaign(t *testing.T) {
-	assertions := assert.New(t)
-	name := "Campaign X"
-	content := "TESTE"
-	contacts := []string{"email1@e.com", "email2@e.com"}
+var (
+	name     = "Campaign X"
+	content  = "TESTE"
+	contacts = []string{"email1@e.com", "email2@e.com"}
+)
 
+func Test_NewCampaign_CreateCampaign(t *testing.T) {
+	assertions := assert.New(t)
 	campaign := NewCampaign(name, content, contacts)
 
 	assertions.Equal(campaign.ID, "1")
 	assertions.Equal(campaign.Name, name)
 	assertions.Equal(campaign.Content, content)
 	assertions.Equal(len(campaign.Contacts), len(contacts))
+}
+
+func Test_NewCampaign_IDIsNotNil(t *testing.T) {
+	assertions := assert.New(t)
+
+	campaign := NewCampaign(name, content, contacts)
+
+	assertions.NotNil(campaign.ID)
+}
+
+func Test_NewCampaign_CreatedOnMustBeNow(t *testing.T) {
+	assertions := assert.New(t)
+	now := time.Now().Add(-time.Minute)
+
+	campaign := NewCampaign(name, content, contacts)
+
+	assertions.NotNil(campaign.CreatedOn)
+	assertions.Greater(campaign.CreatedOn, now)
 }
